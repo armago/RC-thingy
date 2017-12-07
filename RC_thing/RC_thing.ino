9899b6cde3cb4b425e140b455c5a254c468374d7
armago <amago@bancroftschool.org>
2017-12-07 11:33:56 -0500

    add transistor

--- RC_thing/RC_thing.ino
+++ RC_thing/RC_thing.ino
@@ -1,6 +1,7 @@
 int ch31 = 0;
 int ch3 = 0;
 int ch32 = 0;
+int ch33 = 0;
 int mtrSpd = 0;
 # include <Servo.h>
 Servo motor1;
@@ -18,13 +19,13 @@ ch31 = pulseIn(3, HIGH, 25000);
 
 if (ch31 > 0) {
  ch32 = ch31/20;
- ch3 = ch32 - 49;
- 
+ ch33 = ch32 - 49;
+ ch3 = ch33*5;
 Serial.println(ch3);
 }
 
-digitalWrite(8, HIGH);
+analogWrite(9, ch3);
 
-delay(30);
+delay(10);
 }
 
